9e3faac5ba4aad3dadef40e939d67ab953f862bd
James Sweet <james.sweet88@googlemail.com>
2017-01-09 14:05:08 -0500

    Remove old periodic boundary condition test

--- test/particle.cpp
+++ test/particle.cpp
@@ -104,55 +104,6 @@ TEST( ParticleCUDA, ParticleUpdate ) {
 	}
 }
 
-TEST( ParticleCUDA, ParticleUpdatePeriodic ) {
-    std::vector<Particle> input = ReadParticles( "../test/data/particle_periodic_input.dat" );
-	ASSERT_EQ( input.size(), 10 );
-
-	std::vector<Particle> expected = ReadParticles( "../test/data/particle_periodic_expected.dat" );
-	ASSERT_EQ( expected.size(), 10 );
-
-
-	GPU *gpu = NewGPU(10, 0, 0, 0, 0, 0.251327, 0.251327, 0.0, 0.0 );
-	ParticleInit(gpu, 10, &input[0]);
-	ParticleUpdatePeriodic(gpu);
-    Particle *result = ParticleDownload(gpu);
-
-    for( int i = 0; i < 10; i++ ) {
-		for( int k = 0; k < 10; k++ ){
-			if(expected[i].pidx != result[k].pidx) continue;
-			if(expected[i].procidx != result[k].procidx) continue;
-
-			ASSERT_FLOAT_EQ( expected[i].pidx, result[k].pidx ) << "I: " << i;
-			ASSERT_FLOAT_EQ( expected[i].procidx, result[k].procidx ) << "I: " << i;
-
-			for( int j = 0; j < 3; j++ ) {
-				ASSERT_FLOAT_EQ( expected[i].vp[j], result[k].vp[j] ) << "I: " << i << " J: " << j;
-			}
-			for( int j = 0; j < 3; j++ ) {
-				ASSERT_FLOAT_EQ( expected[i].xp[j], result[k].xp[j] ) << "I: " << i << " J: " << j;
-			}
-			for( int j = 0; j < 3; j++ ) {
-				ASSERT_FLOAT_EQ( expected[i].uf[j], result[k].uf[j] ) << "I: " << i << " J: " << j;
-			}
-			for( int j = 0; j < 3; j++ ) {
-				ASSERT_FLOAT_EQ( expected[i].xrhs[j], result[k].xrhs[j] ) << "I: " << i << " J: " << j;
-			}
-			for( int j = 0; j < 3; j++ ) {
-				ASSERT_FLOAT_EQ( expected[i].vrhs[j], result[k].vrhs[j] ) << "I: " << i << " J: " << j;
-			}
-
-			ASSERT_FLOAT_EQ( expected[i].Tp, result[k].Tp ) << "I: " << i;
-			ASSERT_FLOAT_EQ( expected[i].Tprhs_s, result[k].Tprhs_s ) << "I: " << i;
-			ASSERT_FLOAT_EQ( expected[i].Tprhs_L, result[k].Tprhs_L ) << "I: " << i;
-			ASSERT_FLOAT_EQ( expected[i].Tf, result[k].Tf ) << "I: " << i;
-			ASSERT_FLOAT_EQ( expected[i].radius, result[k].radius ) << "I: " << i;
-			ASSERT_FLOAT_EQ( expected[i].radrhs, result[k].radrhs ) << "I: " << i;
-			ASSERT_FLOAT_EQ( expected[i].qinf, result[k].qinf ) << "I: " << i;
-			ASSERT_FLOAT_EQ( expected[i].qstar, result[k].qstar ) << "I: " << i;
-		}
-	}
-}
-
 void CompareParticle(Particle* actual, Particle* expected){
 	if(actual->pidx != expected->pidx) return;
 	if(actual->procidx != expected->procidx) return;
